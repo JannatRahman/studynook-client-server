913421ed569a08886c7278179639548b3f1dc7cd
JannatRahman <you@example.com>
2026-06-03 03:23:44 +0600

    spinner added

--- src/app/all-rooms/[id]/loading.js
+++ src/app/all-rooms/[id]/loading.js
@@ -1,11 +1,11 @@
-import { Spinner } from '@heroui/react';
+
 import React from 'react';
 
 const LoadingPage = () => {
   return (
     <div>
        <div className="flex flex-col items-center gap-2 p-15">
-        <Spinner size="xl" />
+        <h2 className='text-5xl flex items-center justify-center'>Loading...</h2>
         <span className="text-xs text-muted"></span>
       </div>
     </div>
